307b31ffb75baca49fd0346d3d60da0e2d61c486
Steve Goodhill <s.goodhill@protonmail.com>
2024-01-21 14:24:40 +1000

    Update stack name and add app config

--- bin/club-music-cdk.ts
+++ bin/club-music-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ClubmusiccdkStack } from '../lib/club-music-stack';
+import { ClubMusicStack } from '../lib/club-music-stack';
 
 const app = new cdk.App();
-new ClubmusiccdkStack(app, 'ClubmusiccdkStack', {
+new ClubMusicStack(app, 'ClubMusicStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +18,10 @@ new ClubmusiccdkStack(app, 'ClubmusiccdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    env: {
+        account: '289866763058',
+        region: 'ap-southeast-2',
+    }
 });
+
+app.synth();
